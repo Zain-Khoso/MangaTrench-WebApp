a87e933dfe22e528cfd52b8a202fe44917f6870a
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-09 13:29:06 +0500

    Shadcn-ui Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,12 @@ import { Inter } from "next/font/google";
 
 // Local Imports.
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Manga Trench",
@@ -20,7 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}>
+        {children}
+      </body>
     </html>
   );
 }
