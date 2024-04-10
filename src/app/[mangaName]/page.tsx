6854924a1962b9a0a5e6e9f90dd1a04e667986d2
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-10 17:59:42 +0500

    Manga Page

---
+++ src/app/[mangaName]/page.tsx
@@ -0,0 +1,41 @@
+// Lib Imports.
+import Link from 'next/link';
+
+// Local Imports.
+import { connectToDatabase, getModel } from '@/libs/dbClient';
+
+type Props = {
+  params: {
+    mangaName: string;
+  };
+};
+
+export default async function MangaPage({ params: { mangaName } }: Props) {
+  // Waiting for a successful connection to the database.
+  try {
+    await connectToDatabase();
+  } catch {
+    throw new Error(Errors.UNABLE_TO_CONNECT_TO_MONGODB);
+  }
+
+  const manga = await getModel(mangaName)
+    .aggregate()
+    .sort({ ChapterNum: 'ascending' });
+  const chapters = manga.map((item) => {
+    return { chapterNumber: item.ChapterNum, id: item.id };
+  });
+
+  return (
+    <main className="max-w-[100vw] p-8 grid grid-cols-8 gap-4 place-items-center">
+      {chapters.map((item) => (
+        <Link
+          href={`/${mangaName}/chapter-${item.chapterNumber}`}
+          key={item.id}
+          className="bg-teal-800 text-white px-12 py-2 rounded"
+        >
+          Chapter {item.chapterNumber}
+        </Link>
+      ))}
+    </main>
+  );
+}
