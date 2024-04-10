bd8ff4cfb130818e5529d65cff1b9a99bec37c5e
Zain-Khoso <zain.khoso.dev@gmail.com>
2024-04-10 17:00:50 +0500

    SearchBox removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,12 @@
 // Lib Imports.
 import Link from 'next/link';
 
-// Local Imports.
-import Search from './Search';
-
 export default function Navbar() {
   return (
     <nav className="w-full px-8 py-4 bg-teal-600 flex items-center justify-between">
       <h2 className="text-3xl font-bold">
         <Link href="/">Manga Trench</Link>
       </h2>
-
-      <Search />
     </nav>
   );
 }
