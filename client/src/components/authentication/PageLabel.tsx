4fefe4b6c49e466a1fa19acc6aaf23a4c8b97b64
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-08-10 19:47:11 +0500

    update: Google SSO

--- client/src/components/authentication/PageLabel.tsx
+++ client/src/components/authentication/PageLabel.tsx
@@ -1,14 +1,23 @@
 'use client';
 
 // Lib Imports.
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+// Utils.
+import { auth } from '@/utils/firebase';
 
 // Components.
 import { H1 } from '../shadcn/typography';
 
 // Component to dynamically change page name inside authentication layout.
 export default function PageLabel() {
+  const router = useRouter();
   const pathname = usePathname();
+
+  const [user] = useAuthState(auth);
+
+  if (user) router.push('/');
 
   return (
     <H1>{pathname === '/sign-up' ? 'Sign Up' : pathname === '/sign-in' ? 'Sign In' : 'Auth'}</H1>
